02809f5e5a0f0d028694404c91faba776dd70ac4
Thomas Schuh <info@thomas-schuh.com>
2022-04-29 14:41:09 +0200

    custom_order_cancel module added

---
+++ web/modules/custom/custom_order_cancel/src/Form/CancelOrderForm.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Drupal\custom_order_cancel\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * User Form to cancel own order.
+ *
+ * @internal
+ */
+class CancelOrderForm extends FormBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * The current order.
+   *
+   * @var: Drupal\Commerce
+   */
+  protected $order;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'custom_order_cancel_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $order = NULL) {
+
+    $allowed = FALSE;
+
+    $this->order = $order;
+
+    $state = $order->getState()->getValue()['value'];
+    if ($state == 'canceled') {
+      #$form['header'] = [
+      #  '#type' => 'markup',
+      #  '#markup' => $this->t('Storniert'),
+      #];
+      return $form;
+    }
+
+    if ($state == 'completed') {
+      #$form['header'] = [
+      #  '#type' => 'markup',
+      #  '#markup' => $this->t('Versendet'),
+      #];
+      return $form;
+    }
+
+    // Calculate if the order is allowed to cancel.
+    $deadline = 16;
+    $hour = date("H");
+    $date = date("Ymd");
+    $date_yesterday =  date("Ymd", strtotime("yesterday"));
+
+    $hour_created = date("H", $order->getCreatedTime());
+    $date_created = date("Ymd", $order->getCreatedTime());
+
+    if ($hour >= $deadline) {
+      // jetzt ist nach 16 uhr
+      if ($date == $date_created) {
+        // Die Bestellung ist von heute.
+        if ($hour_created >= $deadline) {
+          // Die Bestellung ist von heute nach 16 Uhr.
+          $allowed = TRUE;
+        }
+      }
+
+    } else {
+      // jetzt ist noch vor 16 uhr.
+      if ($date == $date_created) {
+        // Die Bestellung ist von heute.
+        $allowed = TRUE;
+      }
+      if ($date_yesterday == $date_created) {
+        if ($hour_created >= $deadline) {
+          // Die Bestellung ist von gestern nach 16 Uhr.
+          $allowed = TRUE;
+        }
+      }
+    }
+
+    if ($allowed === TRUE) {
+      $form['submit'][1] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Stornieren'),
+        '#button_type' => 'primary',
+        '#attributes' => array('onclick' => 'if(!confirm("Wollen Sie die Bestellung wirklich stornieren? Der Vorgang kann nicht Rückgängig gemacht werden.")){return false;}')
+      ];
+    } else {
+      $form['header'] = [
+        '#type' => 'markup',
+        '#markup' => $this->t('In Produktion'),
+      ];
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $this->order->getState()->applyTransitionById('cancel');
+    $this->order->save();
+
+    return $form;
+  }
+}
---
+++ web/modules/custom/custom_order_cancel/src/Plugin/views/field/CancelOrderViewsField.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\custom_order_cancel\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormBuilder;
+
+/**
+ * A handler to provide a field that is completely custom by the administrator.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("cancel_order_views_field")
+ */
+class CancelOrderViewsField extends FieldPluginBase {
+
+  /**
+   * @var $formBuilder Drupal\Core\Form\FormBuilder
+   */
+  protected $formBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilder $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function usesGroupBy() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Do nothing -- to override the parent query.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+
+    $order = $values->_entity;
+
+    return $this->formBuilder->getForm('Drupal\custom_order_cancel\Form\CancelOrderForm', $order);
+  }
+}
